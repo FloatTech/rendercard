d77a6b5afb89a2355b5602289bcc431c710fe907
方柳煜 <101934327+fangliuyu@users.noreply.github.com>
2022-11-21 11:05:25 +0800

    Update render.go

--- render.go
+++ render.go
@@ -2,13 +2,18 @@
 package rendercard
 
 import (
+	"errors"
+	"fmt"
 	"image"
 	"math/rand"
+	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/Coloured-glaze/gg"
 	"github.com/FloatTech/floatbox/img/writer"
 	"github.com/FloatTech/zbputils/img"
+	"github.com/FloatTech/zbputils/img/text"
 )
 
 const (
@@ -17,6 +22,8 @@ const (
 
 // Titleinfo ...
 type Titleinfo struct {
+	Line int
+
 	Lefttitle    string
 	Leftsubtitle string
 
@@ -31,10 +38,37 @@ type Titleinfo struct {
 	Status bool
 }
 
+type TextCardInfo struct {
+	// 卡片规格:宽度,默认600
+	Width int
+	// 卡片规格:高度,默认由Title+Text内容决定
+	High int
+	// 卡片规格:背景图
+	Imgpath string
+	// 标题字体
+	FontOfTitle string
+	// 正文的字体
+	FontOfText string
+	// 标题规格:标题内容
+	Title string
+	// 标题规格:标题布局[Left|Center|Right],默认Center
+	TitleSetting string
+	// 正文规格:正文内容
+	//
+	// 仅支持类型:String,[]string,Struct{...string}
+	Text any
+	// 正文规格:正文要求
+	//
+	// 当正文为数组时,true为每个元素按行显示,false按空格分割显示;
+	//
+	//当正文为结构体时,true为显示key指
+	DisplaySetting bool
+}
+
 // Drawtitle ...
 func (t Titleinfo) Drawtitle() (imgs image.Image, err error) {
 	// 创建图像
-	canvas := gg.NewContext(int(Imgwight), 30+30+300+(9*(256+30)))
+	canvas := gg.NewContext(int(Imgwight), 30+30+300+(t.Line*(256+30)))
 	canvas.SetRGBA255(240, 240, 240, 255)
 	canvas.Clear()
 
@@ -86,8 +120,11 @@ func (t Titleinfo) Drawtitle() (imgs image.Image, err error) {
 
 // Drawtitledtext ...
 func (t Titleinfo) Drawtitledtext(info []string) (imgs []byte, err error) {
-
-	imgh := len(info)*(38+20) + 220 + 10 + 30 + 10 + 50
+	line := len(info)
+	if line < 6 {
+		line = 6
+	}
+	imgh := line*(32+20) + 220 + 10 + 30 + 10 + 50
 
 	// 创建图像
 	canvas := gg.NewContext(int(Imgwight), imgh)
@@ -216,5 +253,142 @@ func (t Titleinfo) Drawcard() (imgs image.Image, err error) {
 	canvas.DrawString(t.Leftsubtitle, recw/32, (recw*0.475)+recw/6-canvas.FontHeight()/4)
 
 	imgs = canvas.Image()
+	return
+}
+
+// 绘制文字卡片
+func (g TextCardInfo) DrawTextCard() (imgForCard image.Image, imgHigh int, err error) {
+	width := g.Width
+	if width == 0 {
+		width = 600
+	}
+	// 根据宽度获取高度
+	textHigh := 0
+	var textPic image.Image
+	fontOfText := g.FontOfText
+	if fontOfText == "" {
+		fontOfText = text.SakuraFontFile
+	}
+	// 正文数据的类型
+	ref := reflect.TypeOf(g.Text)
+	typeOfTest := ref.Kind()
+	switch typeOfTest {
+	case reflect.String: // 纯文本
+		textImgInfo, testerr := text.Render(fmt.Sprint(g.Text), fontOfText, width-80, 38)
+		if testerr != nil {
+			err = testerr
+			return
+		}
+		textPic = textImgInfo.Image()
+		textHigh = textPic.Bounds().Dy()
+	case reflect.Array: // 数组
+		value := reflect.ValueOf(g.Text)
+		list, ok := value.Interface().([]string) // 断言字符串数组
+		if !ok {
+			err = errors.New("不支持该类型类型")
+			return
+		}
+		textString := ""
+		if g.DisplaySetting {
+			textString = strings.Join(list, "\n")
+		} else {
+			textString = strings.Join(list, " ")
+		}
+		textImgInfo, testerr := text.Render(textString, fontOfText, width-80, 38)
+		if testerr != nil {
+			err = testerr
+			return
+		}
+		textPic = textImgInfo.Image()
+		textHigh = textPic.Bounds().Dy()
+	case reflect.Struct: // 结构体
+		value := reflect.ValueOf(g.Text)
+		var textinfo []string
+		for i := 0; i < ref.NumField(); i++ {
+			if g.DisplaySetting {
+				textinfo = append(textinfo, ref.Field(i).Name)
+			}
+			switch ref.Field(i).Type.Kind() {
+			case reflect.Int:
+				textinfo = append(textinfo, strconv.Itoa(value.Field(i).Interface().(int)))
+			case reflect.Int64:
+				textinfo = append(textinfo, strconv.FormatInt(value.Field(i).Interface().(int64), 10))
+			case reflect.Float64:
+				textinfo = append(textinfo, strconv.FormatFloat(value.Field(i).Interface().(float64), 'f', 3, 64))
+			case reflect.String:
+				textinfo = append(textinfo, value.Field(i).Interface().(string))
+			case reflect.Bool:
+				textinfo = append(textinfo, strconv.FormatBool(value.Field(i).Interface().(bool)))
+			default:
+				err = errors.New("不支持该类型类型")
+				return
+			}
+		}
+		textString := strings.Join(textinfo, "\n")
+		textImgInfo, testerr := text.Render(textString, fontOfText, width-80, 38)
+		if testerr != nil {
+			err = testerr
+			return
+		}
+		textPic = textImgInfo.Image()
+		textHigh = textPic.Bounds().Dy()
+	default:
+		err = errors.New("不支持该类型类型")
+		return
+	}
+	// 计算图片高度
+	imgHigh = g.High
+	if imgHigh == 0 {
+		imgHigh = 30 + 100 + textHigh + 20
+	}
+	// 创建画布
+	canvas := gg.NewContext(width, imgHigh)
+	// 随机背景色
+	if g.Imgpath == "" {
+		canvas.DrawRectangle(0, 0, float64(width), float64(imgHigh))
+		canvas.SetRGBA255(rand.Intn(45)+165, rand.Intn(45)+165, rand.Intn(45)+165, 255)
+		canvas.Fill()
+	} else {
+		banner, err := img.LoadFirstFrame(g.Imgpath, width, imgHigh)
+		if err == nil {
+			canvas.DrawImage(img.Size(banner.Im, width, imgHigh).Im, 0, 0)
+		}
+	}
+	// 标题
+	fontOfTitle := g.FontOfTitle
+	if fontOfTitle == "" {
+		fontOfTitle = text.SakuraFontFile
+	}
+	err = canvas.LoadFontFace(fontOfTitle, 103)
+	if err != nil {
+		return
+	}
+	canvas.SetRGB(0, 0, 0)
+	var titleDx float64 = 10
+	var titleDy float64 = 90
+	switch g.TitleSetting {
+	case "Left":
+		_, titleDy = canvas.MeasureString(g.Title)
+	case "Center":
+		widthOfTilte, hightOfTilte := canvas.MeasureString(g.Title)
+		titleDx = (float64(width) - widthOfTilte) / 2
+		titleDy = hightOfTilte
+	case "Right":
+		widthOfTilte, hightOfTilte := canvas.MeasureString(g.Title)
+		titleDx = float64(width) - widthOfTilte
+		titleDy = hightOfTilte
+	default:
+		err = errors.New("TitleSetting 参数错误")
+		return
+	}
+	canvas.DrawString(g.Title, titleDx, titleDy+10)
+	// 画横线
+	canvas.DrawRoundedRectangle(10, 115, 580, 10, 2.5)
+	canvas.SetRGB(0, 0, 0)
+	canvas.Fill()
+	// 内容
+	canvas.DrawImage(textPic, 10, 130)
+	// 制图
+	imgForCard = canvas.Image()
 	return
 }
